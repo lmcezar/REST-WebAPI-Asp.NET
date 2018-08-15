c8cc6d909426eb2d3ed7222f17a3411441a6d2f6
Lucas Cezar <lmcezar@gmail.com>
2018-08-15 11:45:15 -0300

    Upload dos arquivos inicias do projeto

---
+++ RestServices/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RestServices
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Rota Web API
+            routes.MapHttpRoute(
+                name: "API Default",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = UrlParameter.Optional }
+            );
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ RestServices/Controllers/ClienteController.cs
@@ -0,0 +1,76 @@
+﻿using RestServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestServices.Controllers
+{
+    public class ClienteController : ApiController
+    {
+
+        private readonly Cliente[] Clientes = new Cliente[]
+        {
+            new Cliente
+            {
+                ID = 1,
+                Nome = "Eduardo Pires",
+                Email = "falecom@eduardopires.net.br",
+                Ativo = true
+
+            },
+
+
+            new Cliente
+            {
+                ID = 2,
+                Nome = "Lucas Cezar",
+                Email = "lmcezar@gmail.com",
+                Ativo = true
+            },
+
+            new Cliente
+            {
+                ID = 3,
+                Nome = "Jon Snow",
+                Email = "jon@stark.com.br",
+                Ativo = false
+
+            }
+        };
+
+        // GET: api/Cliente
+        [AcceptVerbs("GET")]
+        public Cliente[] Get()
+        {
+            return Clientes;
+        }
+
+        // GET: api/Cliente/5
+        public Cliente Get(int id)
+        {
+            var clientes = Clientes;
+            //return clientes.SingleOrDefault(x => x.ID);
+            return clientes.SingleOrDefault(x => x.ID == id);
+
+
+        }
+
+        // POST: api/Cliente
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Cliente/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Cliente/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
